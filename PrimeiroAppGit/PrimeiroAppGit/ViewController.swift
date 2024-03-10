85d73c0c66452d83f999c70b7b8b995123f29dc7
Gabriel Mors <gabrielmors210@gmail.com>
2024-03-10 12:15:24 -0400

    adicionei uma label

--- PrimeiroAppGit/PrimeiroAppGit/ViewController.swift
+++ PrimeiroAppGit/PrimeiroAppGit/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Aqui tem uma label :)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Gabriel Escreveu aqui")
