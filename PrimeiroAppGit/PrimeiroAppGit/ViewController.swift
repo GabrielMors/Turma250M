29702fcb8ddd2ceb4c31cd2387c9c1159eb46553
Gabriel Mors <gabrielmors210@gmail.com>
2024-03-10 10:53:43 -0400

    Escrevi um comentario

---
+++ PrimeiroAppGit/PrimeiroAppGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrimeiroAppGit
+//
+//  Created by Gabriel Mors  on 10/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Gabriel Escreveu aqui")
+    }
+
+
+}
+
